3020c17a0738bf8ad1add3fe7420baec3e6adc83
zheng huang <zhenghuang@zhengdeMacBook-Pro.local>
2023-04-14 22:02:31 +0800

    First commit

---
+++ src/views/Create/CreateView.vue
@@ -0,0 +1,210 @@
+<template>
+  <div class="page flex-col">
+    <div class="group_1 flex-col">
+      <div class="box_1 flex-row">
+        <div class="block_1 flex-col"></div>
+        <img
+          class="image_1"
+          referrerpolicy="no-referrer"
+          src="./assets/img/SketchPng8aa99b1140867d6ee6b7145dbf5dd557e27297871db6d8a49f57a70d781f046e.png"
+        />
+        <span class="text_1">create</span>
+        <span class="text_2">Open</span>
+        <span class="text_3">withdraw</span>
+        <img
+          class="label_1"
+          referrerpolicy="no-referrer"
+          src="./assets/img/SketchPngd2150dbf2fea5b177dfc43c79b643d8dc8997d97c2939c8348b5e1459e789d2d.png"
+        />
+        <span class="text_4">0</span>
+        <img
+          class="label_2"
+          referrerpolicy="no-referrer"
+          src="./assets/img/SketchPngf49cc3d4277a41aa4f0079042121c57b35b3740962d8949bffc9b0e33441f4a5.png"
+        />
+        <span class="text_5">0</span>
+        <div class="text-wrapper_1 flex-col"><span class="text_6">Connect&nbsp;teitter</span></div>
+        <div class="text-wrapper_2 flex-col"><span class="text_7">Connect&nbsp;wallet</span></div>
+        <img
+          class="label_3"
+          referrerpolicy="no-referrer"
+          src="./assets/img/SketchPngb4939d70dac305c63e1c51ff76568155ed98c7a93de2a4b9fad15062332202fa.png"
+        />
+        <span class="text_8">英文</span>
+        <img
+          class="thumbnail_1"
+          referrerpolicy="no-referrer"
+          src="./assets/img/SketchPnged80ce90afff850c3deb8f7a4342f592515941bb75340ae93d40c06f26aef150.png"
+        />
+        <div class="block_2 flex-col"><div class="section_1 flex-col"></div></div>
+      </div>
+      <span class="text_9">Token&nbsp;Address</span>
+      <div class="box_2 flex-col justify-end">
+        <div class="text-wrapper_3 flex-col"><span class="text_10">ETH</span></div>
+        <div class="text-wrapper_4">
+          <span class="text_11">0xBitcoin&nbsp;Token</span>
+          <span class="text_12">（</span>
+          <span class="text_13">0xBTC</span>
+          <span class="text_14">）</span>
+        </div>
+        <img
+          class="image_2"
+          referrerpolicy="no-referrer"
+          src="./assets/img/SketchPng7fa34a655b94e435d75528c51a71fc7888ee396fb18c83d866ee144888ffd7b6.png"
+        />
+        <div class="text-wrapper_5">
+          <span class="text_15">0xBitcoin&nbsp;Token</span>
+          <span class="text_16">（</span>
+          <span class="text_17">0xBTC</span>
+          <span class="text_18">）</span>
+        </div>
+        <img
+          class="image_3"
+          referrerpolicy="no-referrer"
+          src="./assets/img/SketchPng7fa34a655b94e435d75528c51a71fc7888ee396fb18c83d866ee144888ffd7b6.png"
+        />
+        <div class="text-wrapper_6">
+          <span class="text_19">0xBitcoin&nbsp;Token</span>
+          <span class="text_20">（</span>
+          <span class="text_21">0xBTC</span>
+          <span class="text_22">）0xBitcoin&nbsp;Token（0xBTC）</span>
+        </div>
+        <img
+          class="image_4"
+          referrerpolicy="no-referrer"
+          src="./assets/img/SketchPng7fa34a655b94e435d75528c51a71fc7888ee396fb18c83d866ee144888ffd7b6.png"
+        />
+        <div class="text-wrapper_7">
+          <span class="text_23">0xBitcoin&nbsp;Token</span>
+          <span class="text_24">（</span>
+          <span class="text_25">0xBTC</span>
+          <span class="text_26">
+            ）0xBitcoin&nbsp;Token（0xBTC）0xBitcoin&nbsp;Token（0xBTC）0xBitcoin&nbsp;Token（0xBTC）
+          </span>
+        </div>
+        <img
+          class="image_5"
+          referrerpolicy="no-referrer"
+          src="./assets/img/SketchPng7fa34a655b94e435d75528c51a71fc7888ee396fb18c83d866ee144888ffd7b6.png"
+        />
+        <div class="text-wrapper_8">
+          <span class="text_27">0xBitcoin&nbsp;Token</span>
+          <span class="text_28">（</span>
+          <span class="text_29">0xBTC</span>
+          <span class="text_30">）</span>
+        </div>
+        <img
+          class="image_6"
+          referrerpolicy="no-referrer"
+          src="./assets/img/SketchPng7fa34a655b94e435d75528c51a71fc7888ee396fb18c83d866ee144888ffd7b6.png"
+        />
+        <div class="text-wrapper_9">
+          <span class="text_31">0xBitcoin&nbsp;Token</span>
+          <span class="text_32">（</span>
+          <span class="text_33">0xBTC</span>
+          <span class="text_34">
+            ）0xBitcoin&nbsp;Token（0xBTC）0xBitcoin&nbsp;Token（0xBTC）0xBitcoin&nbsp;Token（0xBTC）0xBitcoin&nbsp;Token（0xBTC）0xBitcoin&nbsp;Token（0xBTC）
+          </span>
+        </div>
+        <img
+          class="image_7"
+          referrerpolicy="no-referrer"
+          src="./assets/img/SketchPng7fa34a655b94e435d75528c51a71fc7888ee396fb18c83d866ee144888ffd7b6.png"
+        />
+        <div class="text-wrapper_10">
+          <span class="text_35">0xBitcoin&nbsp;Token</span>
+          <span class="text_36">（</span>
+          <span class="text_37">0xBTC</span>
+          <span class="text_38">）</span>
+        </div>
+      </div>
+      <div class="text-wrapper_11 flex-row justify-between">
+        <span class="text_39">Amout</span>
+        <span class="text_40">Number&nbsp;Of&nbsp;Packets</span>
+      </div>
+      <div class="box_3 flex-row justify-between">
+        <div class="text-wrapper_12 flex-col"><span class="text_41">0.005</span></div>
+        <div class="text-wrapper_13 flex-col"><span class="text_42">10</span></div>
+      </div>
+      <div class="box_4 flex-row justify-between">
+        <span class="text_43">Advanced&nbsp;Options</span>
+        <img
+          class="thumbnail_2"
+          referrerpolicy="no-referrer"
+          src="./assets/img/SketchPngd5a522db76570293d0fdc66e03b85393e9f273b8812881faabab2e8a8f55d64a.png"
+        />
+      </div>
+      <span class="text_44">Your&nbsp;Twitter</span>
+      <div class="box_5 flex-col justify-end">
+        <div class="group_2 flex-row justify-between">
+          <div class="image-text_1 flex-row justify-between">
+            <img
+              class="label_4"
+              referrerpolicy="no-referrer"
+              src="./assets/img/SketchPngeb5e0d1bc37b86649cb23471b1c754a9742bdc55f73966f5563119160c0d19d4.png"
+            />
+            <span class="text-group_1">For&nbsp;Everyone</span>
+          </div>
+          <div class="image-text_2 flex-row justify-between">
+            <div class="box_6 flex-col"></div>
+            <span class="text-group_2">For&nbsp;Appointed&nbsp;/&nbsp;lottery</span>
+          </div>
+        </div>
+        <div class="group_3 flex-row justify-between">
+          <div class="image-text_3 flex-row justify-between">
+            <img
+              class="label_5"
+              referrerpolicy="no-referrer"
+              src="./assets/img/SketchPngeb5e0d1bc37b86649cb23471b1c754a9742bdc55f73966f5563119160c0d19d4.png"
+            />
+            <span class="text-group_3">Random&nbsp;Allocation</span>
+          </div>
+          <div class="image-text_4 flex-row justify-between">
+            <div class="box_7 flex-col"></div>
+            <span class="text-group_4">Equal&nbsp;Distribation</span>
+          </div>
+        </div>
+        <span class="text_45">Time&nbsp;Limit</span>
+        <div class="group_4 flex-row justify-between">
+          <div class="text-wrapper_14 flex-col"><span class="text_46">3</span></div>
+          <span class="text_47">days</span>
+          <div class="text-wrapper_15 flex-col"><span class="text_48">Please&nbsp;enter</span></div>
+          <span class="text_49">hours</span>
+          <div class="text-wrapper_16 flex-col"><span class="text_50">Please&nbsp;enter</span></div>
+          <span class="text_51">minutes</span>
+          <div class="text-wrapper_17 flex-col"><span class="text_52">Please&nbsp;enter</span></div>
+          <span class="text_53">seconds</span>
+        </div>
+        <div class="text-wrapper_18 flex-col"><span class="text_54">Create</span></div>
+        <div class="text-wrapper_19 flex-row">
+          <span class="text_55">Home</span>
+          <span class="text_56">Feature</span>
+          <span class="text_57">Price</span>
+          <span class="text_58">Common&nbsp;Problem</span>
+          <span class="text_59">About</span>
+          <span class="text_60">©&nbsp;2022&nbsp;Company,&nbsp;Inc</span>
+        </div>
+      </div>
+      <div class="box_8 flex-row">
+        <span class="text_61">&#64;</span>
+        <img
+          class="image_8"
+          referrerpolicy="no-referrer"
+          src="./assets/img/SketchPng4004dcc8a00e49a80a87bb96abe8fe4d6f1ee74585cc22819cd755c12ba59304.png"
+        />
+        <span class="text_62">therandomodao</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      constants: {}
+    };
+  },
+  methods: {}
+};
+</script>
+<style scoped lang="css" src="./assets/index.css" />
